7453e0df79e488bac41199eaeb8ef4bfa39671f4
Vapor <vapor@vaporwaresoftworks.com>
2025-11-27 17:43:44 -0600

    updates to updater to restore functionality broken by grok

--- tools/Toolchain_Zig.cmake
+++ tools/Toolchain_Zig.cmake
@@ -1,8 +1,5 @@
-cd ~/_/SneakernetStudio
-cat > tools/Toolchain_Zig.cmake <<'EOF'
 cmake_minimum_required(VERSION 3.20)
 
 set(ZIG_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/../../tools/zig")
 set(CMAKE_C_COMPILER "${ZIG_ROOT}/zig" cc)
 set(CMAKE_CXX_COMPILER "${ZIG_ROOT}/zig" c++)
-EOF
