570e6b43bfa38070c49cb13e9ca0b24867793eda
Vapor <vapor@vaporwaresoftworks.com>
2025-11-27 17:30:36 -0600

    typo fixed in Toolchain_Zig.cmake

--- tools/Toolchain_Zig.cmake
+++ tools/Toolchain_Zig.cmake
@@ -1,5 +1,5 @@
 cmake_minimum_required(VERSION 3.20)
 
 set(ZIG_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/../../tools/zig")
-set(CMAKE_C_COMPILER "${ZIG_ROOT}/zig cc")
+set(CMAKE_C_COMPILER "${ZIG_ROOT}/zig" cc")
 set(CMAKE_CXX_COMPILER "${ZIG_ROOT}/zig c++")
