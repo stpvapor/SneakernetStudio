bb77ca45dc736214ec55760ed54e180c3237e3f0
Vapor <vapor@vaporwaresoftworks.com>
2025-11-27 18:28:57 -0600

    starting from scratch with build tools

--- tools/Toolchain_Zig.cmake
+++ tools/Toolchain_Zig.cmake
@@ -3,6 +3,3 @@ cmake_minimum_required(VERSION 3.20)
 set(ZIG_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/../../tools/zig")
 set(CMAKE_C_COMPILER "${ZIG_ROOT}/zig" cc)
 set(CMAKE_CXX_COMPILER "${ZIG_ROOT}/zig" c++)
-
-set(CMAKE_C_COMPILER_FORCED TRUE)
-set(CMAKE_CXX_COMPILER_FORCED TRUE)
